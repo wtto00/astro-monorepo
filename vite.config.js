029b91a78daf24de0bcd02acca39e2268db0d876
p_pbluewang <p_pbluewang@tencent.com>
2022-04-01 16:22:55 +0800

    配置打包输出命令在根目录dist，而非每个项目中的文件夹内

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,8 @@ export default defineConfig({
       [process.env.PROJECT_NAME]: fileURLToPath(new URL(`./src/packages/${process.env.PROJECT_NAME}`, import.meta.url)),
     },
   },
+  build: {
+    outDir: fileURLToPath(new URL('./dist', import.meta.url)),
+    emptyOutDir: false,
+  },
 });
